15931a2c1ba8f0734fa246c5d1ec732004d70bc1
Shahed341 <iamshahedd@gmail.com>
2025-07-09 22:33:57 -0600

    Scroll to top logic added for every new page open.

--- progym-frontend/src/App.js
+++ progym-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop'; // ✅ Scroll reset on route change
 import { UserContext } from './context/UserContext';
 
 import Home from './pages/Home';
@@ -30,6 +31,7 @@ function App() {
 
   return (
     <>
+      <ScrollToTop /> {/* ✅ Ensures scroll reset on route change */}
       <Navbar />
 
       <div
@@ -40,23 +42,28 @@ function App() {
         }}
       >
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<Home user={user} />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/upgrade" element={<Upgrade />} />
+
+          {/* General Tools */}
           <Route path="/track-workout" element={<TrackWorkout />} />
           <Route path="/exercises" element={<Exercises />} />
           <Route path="/supplement-guide" element={<SupplementGuide />} />
           <Route path="/bulking" element={<BulkingStrategy />} />
           <Route path="/cutting" element={<CuttingStrategy />} />
+
+          {/* Premium Tools */}
           <Route path="/premium" element={<PremiumPage />} />
           <Route path="/premium/progress-charts" element={<ProgressCharts />} />
           <Route path="/premium/gymbot" element={<GymBot />} />
           <Route path="/premium/meal-planner" element={<MealPlanner />} />
           <Route path="/premium/supplements" element={<Supplements />} />
           <Route path="/premium/workouts" element={<Workouts />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
         </Routes>
       </div>
 
